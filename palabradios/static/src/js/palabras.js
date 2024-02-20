0708872640700b2d600b6f50faa4914820af779e
DarioMachado <115183065+DarioMachado@users.noreply.github.com>
2024-02-21 00:14:13 +0100

    Update palabras.js

--- palabradios/static/src/js/palabras.js
+++ palabradios/static/src/js/palabras.js
@@ -1,11 +1,32 @@
-// Wait for the DOM content to be fully loaded
 document.addEventListener("DOMContentLoaded", function() {
-    // Create a text node with the text you want to append
-    var newText = document.createTextNode("Y entonces jesucristo dijo, me cago en tu puta madre" );
-
-    // Get a reference to the body element
+    var divElement = document.createElement("div");
     var body = document.body;
+    body.insertBefore(divElement, body.firstChild);
 
-    // Insert the new text node as the first child of the body element
-    body.insertBefore(newText, body.firstChild);
+    divElement.style.backgroundColor = "black";
+    divElement.style.color = "white";
+    divElement.style.fontSize = "14px";
+
+    function cambiarFrase() {
+        var phrases = [
+            "(Génesis 17:11) Circuncidaréis, pues, la carne de vuestro prepucio, y será por señal del pacto entre mí y vosotros.",
+            "(Deuteronomio 10:16) Circuncidad, pues, el prepucio de vuestro corazón, y no endurezcáis más vuestra cerviz.",
+            "(Deuteronomio 23:17) No haya ramera de entre las hijas de Israel, ni haya sodomita de entre los hijos de Israel.",
+            "(Salmo 137:9) ¡Dichoso el que agarre a tus pequeños y los estrelle contra las rocas!",
+            "(2 Reyes 2:24) Y mirando él atrás, los vio, y los maldijo en el nombre de Jehová. Y salieron dos osos del monte, y despedazaron de ellos a cuarenta y dos muchachos.",
+            "(Mateo 9:12) Al oír esto Jesús, les dijo: Los sanos no tienen necesidad de médico, sino los enfermos.",
+            "(Abraham Mateo 19:98) Sexy señorita",
+            "(Juan 11:25) Le dijo Jesús: Yo soy la resurrección y la vida; el que cree en mí, aunque esté muerto, vivirá."
+
+        ];
+
+        var randomIndex = Math.floor(Math.random() * phrases.length);
+        var randomPhrase = phrases[randomIndex];
+
+        divElement.textContent = randomPhrase;
+    }
+
+    cambiarFrase();
+
+    setInterval(cambiarFrase, 10000);
 });
