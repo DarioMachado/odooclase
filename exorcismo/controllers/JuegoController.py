44375a6623623a9bb3c5f151015f4fc0bc058dee
Darío Machado <dnmachad099@gmail.com>
2024-02-22 16:04:49 +0100

    todos los problemas de cors arreglados

--- exorcismo/controllers/JuegoController.py
+++ exorcismo/controllers/JuegoController.py
@@ -1,13 +1,35 @@
 from odoo import http
 from odoo.http import request
+from odoo import fields
 
 class JuegoController(http.Controller):
 
-    @http.route('/exorcismo/insertar', type='json', auth='user')
-    def insert_score(self, name, score):
-        game_score_obj = request.env['exorcismo.puntuacion']
-        game_score_obj.create({'name': name, 'score': score})
-        return {'success': True}
+    @http.route('/exorcismo/insertar', type='json', auth='public', methods=['POST', 'OPTIONS'], cors='*')
+    def your_api_endpoint(self, **post):
+        if request.httprequest.method == 'OPTIONS':
+            # Handle OPTIONS request
+            response = http.Response()
+            response.headers.add('Access-Control-Allow-Origin', 'https://htmlpreview.github.io/')
+            response.headers.add('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+            response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+            return response.render()
 
+        # Actual logic for handling POST request
+        try:
 
+            json_data = request.jsonrequest
 
+            name = json_data.get('name')
+            date = fields.Date.from_string(json_data.get('date'))
+            score = json_data.get('score')
+
+            # Create a record in the database
+            new_record = request.env['exorcismo.puntuacion'].sudo().create({
+                'name': name,
+                'date': date,
+                'score': score
+            })
+
+            return {"result": "Data inserted successfully"+name}
+        except Exception as e:
+            return {"error": str(e)}
--- exorcismo/models/models.py
+++ exorcismo/models/models.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 class Exorcismo(models.Model):
     _name = 'exorcismo.puntuacion'
-    _description = 'La puntuación del exorcismo, yup...'
+    _description = 'La puntuación del exorcismo'
 
     name = fields.Char(string='Name')
     date = fields.Date(string='Date')
