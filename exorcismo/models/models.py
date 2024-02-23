7a5bc798000617d81cb104eb01410e079c1d3454
Darío Machado <dnmachad099@gmail.com>
2024-02-23 01:55:24 +0100

    cero progreso

--- exorcismo/models/models.py
+++ exorcismo/models/models.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Exorcismo(models.Model):
     _name = 'exorcismo.puntuacion'
@@ -7,3 +7,20 @@ class Exorcismo(models.Model):
     name = fields.Char(string='Name')
     date = fields.Date(string='Date')
     score = fields.Integer(string='Score')
+
+
+    def jugar(self):
+        # Add your code here to open a modal view with an iframe
+        view_id = self.env.ref('exorcismo.your_modal_view_id').id
+        return {
+            'name': 'Modal View with Iframe',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'ir.actions.act_window',
+            'target': 'new',
+            'views': [(view_id, 'form')],
+            'type': 'ir.actions.act_window',
+            'context': {
+                'default_iframe_url': 'https://htmlpreview.github.io/?https://github.com/DarioMachado/odooclase/blob/main/index.html'  # URL for your iframe
+            }
+        }
