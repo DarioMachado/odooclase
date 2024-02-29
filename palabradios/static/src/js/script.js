cea21edb1eb0bba3a14df74c9c82950ceb334c03
Darío Machado <dnmachad099@gmail.com>
2024-02-29 22:24:07 +0100

    ara si

--- palabradios/static/src/js/script.js
+++ palabradios/static/src/js/script.js
@@ -11,14 +11,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function cambiarFrase() {
         var phrases = [
-            "(Génesis 17:11) Circuncidaréis, pues, la carne de vuestro prepucio, y será por señal del pacto entre mí y vosotros.",
+            "(Proverbios 11:25) El que es generoso prospera; el que reanima será reanimado.",
             "(Deuteronomio 10:16) Circuncidad, pues, el prepucio de vuestro corazón, y no endurezcáis más vuestra cerviz.",
             "(Deuteronomio 23:17) No haya ramera de entre las hijas de Israel, ni haya sodomita de entre los hijos de Israel.",
             "(Salmo 137:9) ¡Dichoso el que agarre a tus pequeños y los estrelle contra las rocas!",
             "(2 Reyes 2:24) Y mirando él atrás, los vio, y los maldijo en el nombre de Jehová. Y salieron dos osos del monte, y despedazaron de ellos a cuarenta y dos muchachos.",
             "(Mateo 9:12) Al oír esto Jesús, les dijo: Los sanos no tienen necesidad de médico, sino los enfermos.",
             "(Abraham Mateo 19:98) Sexy señorita",
-            "(Juan 11:25) Le dijo Jesús: Yo soy la resurrección y la vida; el que cree en mí, aunque esté muerto, vivirá."
+            "(Juan 11:25) Le dijo Jesús: Yo soy la resurrección y la vida; el que cree en mí, aunque esté muerto, vivirá.",
+            "(Eclesiástes 1:18) Francamente, mientras más sabiduría, más problemas; mientras más se sabe, más se sufre.",
+            "(Judas 1:2) Que reciban misericordia, paz y amor en abundancia."
 
         ];
 
